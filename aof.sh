edbf5418c7e2c176fab3cbbb9cba7a80a61a7c86
YasyaKarasu <oycy0306@hotmail.com>
2023-09-15 10:11:31 +0800

    add test for expire set of aof

--- aof.sh
+++ aof.sh
@@ -49,4 +49,22 @@ else
     echo -e "[Write AOF Log Test 3] \x1b[31mFAILED\x1b[0m"
 fi
 
+cargo run --bin client "127.0.0.1:19261" set key4 value4 5
+cargo run --bin client "127.0.0.1:19261" get key4 | grep -q "value4"
+if test $? -eq 0
+then
+    echo -e "[Write AOF Log Test 4] \x1b[32mok\x1b[0m"
+else
+    echo -e "[Write AOF Log Test 4] \x1b[31mFAILED\x1b[0m"
+fi
+
+sleep 5
+cargo run --bin client "127.0.0.1:19261" get key4 | grep -q "key not found"
+if test $? -eq 0
+then
+    echo -e "[Write AOF Log Test 5] \x1b[32mok\x1b[0m"
+else
+    echo -e "[Write AOF Log Test 5] \x1b[31mFAILED\x1b[0m"
+fi
+
 kill $(lsof -t -i:19261)
